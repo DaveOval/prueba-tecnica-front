10d0251375ee487dc3790ef330da376529926877
David Vazquez <dave_u@outlook.com>
2025-05-20 13:34:35 -0600

    Enhance layout styles for improved responsiveness
    - Added full height and overflow hidden classes to the main body in RootLayout and AuthLayout components.
    - Updated HTML and body elements to ensure consistent styling across the application.

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -2,7 +2,7 @@ import type { ReactNode } from 'react';
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
     return(
-        <main className="flex h-screen w-screen items-center justify-center">
+        <main className="flex min-h-screen w-full items-center justify-center overflow-hidden">
             { children }
         </main>
     )
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,9 +8,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="h-full">
       <ReduxProvider>
-        <body>
+        <body className="h-full overflow-hidden">
           <Toaster richColors position="top-center" />
           { children }
         </body>
