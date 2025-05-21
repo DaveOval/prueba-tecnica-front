9662fb9c5b4cea5eda2173a604772d12efaea636
David Vazquez <dave_u@outlook.com>
2025-05-21 08:36:39 -0600

    Add SVG icons for UI components and enhance Header and Sidebar functionality
    - Introduced new SVG files for close, images, logout, menu, upload, and user icons to improve UI aesthetics.
    - Updated Header component to include a toggle button for the sidebar with dynamic icon rendering.
    - Enhanced Sidebar component with links to images and upload sections, incorporating new SVG icons for better navigation.
    - Adjusted styles for improved layout and responsiveness in both Header and Sidebar components.

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -1,14 +1,72 @@
+'use client';
 import { RootState } from "@/store"
+import { toggleSidebar } from "@/store/slices/uiSlice";
+import Image from "next/image"
+import { useDispatch, useSelector } from "react-redux";
+
 export default function Header() {
+
+    const { isSidebarOpen } = useSelector((state: RootState) => state.ui);
+    const { user } = useSelector((state: RootState) => state.auth);
+    const dispatch = useDispatch();
+
     return(
         <header 
             className="flex w-full h-16 items-center justify-between p-4"
             style={{
-                border: '1px solid #e0e0e081',
                 borderRadius: '15px',
+                marginTop: '10px',
+                marginBottom: '10px',
             }}
         >
-            <h1>Header</h1>
+            <button 
+                onClick={() => dispatch(toggleSidebar())}
+                className="flex items-center justify-center"
+                style={{
+                    width: '50px',
+                    height: '50px',
+                    backgroundColor: 'transparent',
+                    border: 'none',
+                    cursor: 'pointer',  
+                }}
+            >
+                {
+                    isSidebarOpen ? (
+                        <Image src="/menu.svg" alt="menu" width={30} height={30} />
+                    ) : (
+                        <Image src="/close.svg" alt="menu" width={30} height={30} />
+                    )
+                }
+            </button>
+            <div 
+                className="flex items-center justify-center"
+                style={{
+                    color: '#e0e0e081',
+                    gap: '10px',
+                }}
+            >
+                <p>
+                    <span 
+                    style={{
+                        color: '#ffffff',
+                        fontWeight: 'bold',
+                    }}>Welcome</span> 
+                    {user?.first_name} {user?.last_name} 
+                </p>
+                <Image 
+                    src="/user.svg" 
+                    alt="user" 
+                    width={30} 
+                    height={30} 
+                    style={{
+                        borderRadius: '50%',
+                        marginRight: '10px',
+                        padding: '5px',
+                        border: '3px solid #e0e0e081',
+                    }}
+                />
+                
+            </div>
         </header>
     )
 }
--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -12,7 +12,7 @@ export default function Sidebar() {
         <aside 
             className="flex h-full min-h-screen flex-col items-center justify-between gap-4 overflow-y-auto  p-4 transition-all duration-300 py-4"
             style={{
-                width: !isSidebarOpen ? '260px' : '50px',
+                width: !isSidebarOpen ? '260px' : '70px',
             }}
         >
             <div className="flex flex-col items-center justify-center gap-4 w-full">
@@ -21,17 +21,95 @@ export default function Sidebar() {
                     style={{
                         width: '95%',
                         border: '1px solid #e0e0e081',
-                        borderRadius: '15px',
+                        borderRadius: '10px',
+                        marginTop: '10px',
                     }}
                     className="flex items-center justify-center"
                 >
                     <Image
                         src="/logo.svg" 
                         alt="logo" 
-                        width={80} 
-                        height={80}
+                        width={ !isSidebarOpen ? 70 : 60 } 
+                        height={ !isSidebarOpen ? 70 : 60 }
                     />
                 </Link>
+                <section
+                    style={{
+                        width: '95%',
+                        border: '1px solid #e0e0e081',
+                        borderRadius: '10px',
+                        marginTop: '10px',
+                    }}
+                    className="h-[calc(100vh-200px)] "
+                >
+                    <Link
+                        href={"/my-images"}
+                        className="flex items-center justify-around gap-4"
+                        >
+                        <Image
+                            src="/images.svg"
+                            alt="images"
+                            width={ 40 }
+                            height={ 40 }
+                            className="transition-all duration-300"
+                        />
+                        <p 
+                            style={{
+                                color: '#ffffff',
+                                fontWeight: 'bold',
+                                textDecoration: 'none !important',
+                                textUnderlineOffset: 'none',
+                                display: !isSidebarOpen ? 'block' : 'none',
+                            }}
+                        >
+                            Images
+                        </p>
+                    </Link>
+                    <Link
+                        href={"/dashboard"}
+                        className="flex items-center justify-around gap-4"
+                        >
+                        <Image
+                            src="/upload.svg"
+                            alt="upload"
+                            width={ 40 }
+                            height={ 40 }
+                            className="transition-all duration-300"
+                        />
+                        <p 
+                            style={{
+                                color: '#ffffff',
+                                fontWeight: 'bold',
+                                textDecoration: 'none !important',
+                                textUnderlineOffset: 'none',
+                                display: !isSidebarOpen ? 'block' : 'none',
+                            }}
+                        >
+                            Upload
+                        </p>
+                    </Link>
+                    <button
+                        className="flex items-center justify-around gap-4"
+                        style={{
+                            width: '100%',
+                            border: 'none',
+                            background: 'none',
+                            cursor: 'pointer',
+                        }}
+                        onClick={() => {
+                            console.log('Logout');
+                        }}
+                    >
+                        <Image 
+                            src="/logout.svg"
+                            alt="logout"
+                            width={ 40 }
+                            height={ 40 }
+                            className="transition-all duration-300"
+                        />
+
+                    </button>
+                </section>
             </div>
         </aside>
     )
