b93a9c88b10a236598f0a3f4374a645f53a17cab
David Vazquez <dave_u@outlook.com>
2025-05-21 13:43:52 -0600

    Refactor Sidebar component for improved layout and styling
    - Adjusted sidebar width for better responsiveness.
    - Updated link styles and added hover effects for navigation items.
    - Reorganized component structure to enhance readability and maintainability.
    - Removed deprecated code and streamlined logout functionality.

--- src/components/ui/Sidebar.tsx
+++ src/components/ui/Sidebar.tsx
@@ -22,111 +22,128 @@ export default function Sidebar() {
         <aside 
             className="flex h-full min-h-screen flex-col items-center justify-between gap-4 overflow-y-auto  p-4 transition-all duration-300 py-4"
             style={{
-                width: !isSidebarOpen ? '260px' : '70px',
+                width: !isSidebarOpen ? '200px' : '70px',
             }}
         >
-            <div className="flex flex-col items-center justify-center gap-4 w-full">
+            <div className="flex flex-col items-center justify-center gap-4 w-full ">
                 <Link 
                     href="/dashboard"
                     style={{
                         width: '95%',
-                        border: '1px solid #e0e0e081',
+                        border: '1px solid #27272A',
                         borderRadius: '10px',
                         marginTop: '10px',
                     }}
-                    className="flex items-center justify-center"
+                    className="flex items-center justify-center container_color"
                 >
                     <Image
                         src="/logo.svg" 
                         alt="logo" 
-                        width={ !isSidebarOpen ? 70 : 60 } 
-                        height={ !isSidebarOpen ? 70 : 60 }
+                        width={ !isSidebarOpen ? 50 : 40 } 
+                        height={ !isSidebarOpen ? 50 : 40 }
                     />
                 </Link>
                 <section
                     style={{
                         width: '95%',
-                        border: '1px solid #e0e0e081',
+                        border: '1px solid #27272A',
                         borderRadius: '10px',
                         marginTop: '10px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                        position: 'relative',
+                        height: 'calc(100vh - 200px)',
                     }}
-                    className="h-[calc(100vh-200px)] "
+                    className="container_color"
                 >
-                    <Link
-                        href={"/my-images"}
-                        className="flex items-center justify-around gap-4"
-                        >
-                        <Image
-                            src="/images.svg"
-                            alt="images"
-                            width={ 40 }
-                            height={ 40 }
-                            className="transition-all duration-300"
-                        />
-                        <p 
-                            style={{
-                                color: '#ffffff',
-                                fontWeight: 'bold',
-                                textDecoration: 'none !important',
-                                textUnderlineOffset: 'none',
-                                display: !isSidebarOpen ? 'block' : 'none',
-                            }}
-                        >
-                            Images
-                        </p>
-                    </Link>
-                    <Link
-                        href={"/dashboard"}
-                        className="flex items-center justify-around gap-4"
-                        >
-                        <Image
-                            src="/upload.svg"
-                            alt="upload"
-                            width={ 40 }
-                            height={ 40 }
-                            className="transition-all duration-300"
-                        />
-                        <p 
-                            style={{
-                                color: '#ffffff',
-                                fontWeight: 'bold',
-                                textDecoration: 'none !important',
-                                textUnderlineOffset: 'none',
-                                display: !isSidebarOpen ? 'block' : 'none',
-                            }}
-                        >
-                            Upload
-                        </p>
-                    </Link>
-                    <button
-                        className="flex items-center justify-around gap-4"
+                    <div
                         style={{
-                            width: '100%',
-                            border: 'none',
-                            background: 'none',
-                            cursor: 'pointer',
+                            marginTop: '20px',
+                            width: '90%',
+                            height: '96%',
+                            display: 'flex',
+                            flexDirection: 'column',
                         }}
-                        onClick={handleLogoutClick}
                     >
-                        <Image 
-                            src="/logout.svg"
-                            alt="logout"
-                            width={ 40 }
-                            height={ 40 }
-                            className="transition-all duration-300"
-                        />
-                        <p 
+                        <div className="flex flex-col gap-4">
+                            <Link
+                                href={"/my-images"}
+                                className="flex items-center justify-around p-2 rounded-lg transition-all duration-300 hover:bg-[#27272A] hover:scale-105"
+                            >
+                                <Image
+                                    src="/images.svg"
+                                    alt="images"
+                                    width={ 40 }
+                                    height={ 40 }
+                                    className="transition-all  duration-300"
+                                />
+                                <p 
+                                    style={{
+                                        color: '#ededee',
+                                        fontWeight: 'bold',
+                                        textDecoration: 'none !important',
+                                        textUnderlineOffset: 'none',
+                                        display: !isSidebarOpen ? 'block' : 'none',
+                                    }}
+                                >
+                                    Images
+                                </p>
+                            </Link>
+                            <Link
+                                href={"/dashboard"}
+                                className="flex items-center justify-around p-2 rounded-lg transition-all duration-300 hover:bg-[#27272A] hover:scale-105"
+                            >
+                                <Image
+                                    src="/upload.svg"
+                                    alt="upload"
+                                    width={ 40 }
+                                    height={ 40 }
+                                    className="transition-all duration-300"
+                                />
+                                <p 
+                                    style={{
+                                        color: '#ffffff',
+                                        fontWeight: 'bold',
+                                        textDecoration: 'none !important',
+                                        textUnderlineOffset: 'none',
+                                        display: !isSidebarOpen ? 'block' : 'none',
+                                    }}
+                                >
+                                    Upload
+                                </p>
+                            </Link>
+                        </div>
+                        <button
+                            className="flex items-center justify-around mt-auto mb-4 p-2 rounded-lg transition-all duration-300 hover:bg-[#27272A] hover:scale-105"
                             style={{
-                                color: '#ffffff',
-                                fontWeight: 'bold',
-                                textDecoration: 'none !important',
-                                textUnderlineOffset: 'none',
-                                display: !isSidebarOpen ? 'block' : 'none',
+                                width: '100%',
+                                border: 'none',
+                                background: 'none',
+                                cursor: 'pointer',
                             }}
+                            onClick={handleLogoutClick}
                         >
-                            Logout
-                        </p>
-                    </button>
+                            <Image 
+                                src="/logout.svg"
+                                alt="logout"
+                                width={ 40 }
+                                height={ 40 }
+                                className="transition-all duration-300"
+                            />
+                            <p 
+                                style={{
+                                    color: '#ffffff',
+                                    fontWeight: 'bold',
+                                    textDecoration: 'none !important',
+                                    textUnderlineOffset: 'none',
+                                    display: !isSidebarOpen ? 'block' : 'none',
+                                }}
+                            >
+                                Logout
+                            </p>
+                        </button>
+                    </div>
                 </section>
             </div>
         </aside>
